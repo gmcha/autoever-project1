0190709b24ba651603540b06e56e5c8de17a59d5
Ga Min Cha <prettygamin@naver.com>
2026-02-28 17:33:56 +0900

    추가: Footer 컴포넌트

---
+++ gamin/portfolio/src/components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+function Footer() {
+  return (
+    <div>
+      <hr />
+      <p>Copyright 2026. [개발자명] all rights reserved.</p>
+    </div>
+  );
+}
+
+export default Footer;
--- gamin/portfolio/src/pages/Home/Home.tsx
+++ gamin/portfolio/src/pages/Home/Home.tsx
@@ -1,5 +1,6 @@
 import About from "../../components/About/About";
 import Experience from "../../components/Experience/Experience";
+import Footer from "../../components/Footer/Footer";
 import Skills from "../../components/Skills/Skills";
 import TopBlogs from "../../components/TopBlogs/TopBlogs";
 import TopProjects from "../../components/TopProjects/TopProjects";
@@ -12,6 +13,7 @@ function Home() {
       <Experience />
       <TopProjects />
       <TopBlogs />
+      <Footer />
     </div>
   );
 }
